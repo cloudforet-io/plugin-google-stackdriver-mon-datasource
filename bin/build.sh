efeb29af8f3d655be948d599f877acb03fdcbb2e
dongyo <dkang@mz.co.kr>
2020-09-24 19:13:22 +0900

    update for stackdriver - remove metric_info.json - update stackdriver as interface

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Build a docker image
 cd ..
-docker build -t pyengine/aws-cloudwatch . --no-cache
-docker tag pyengine/aws-cloudwatch pyengine/google-cloud-stackdriver:1.0
-docker tag pyengine/aws-cloudwatch spaceone/google-cloud-stackdriver:1.0
+docker build -t pyengine/google-cloud-stackdriver . --no-cache
+docker tag pyengine/google-cloud-stackdriver pyengine/google-cloud-stackdriver:1.0
+docker tag pyengine/google-cloud-stackdriver spaceone/google-cloud-stackdriver:1.0
