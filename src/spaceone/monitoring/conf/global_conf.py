e73021da20d27355c17e8b8c99b117dc83bd70e9
D.Kang <dkang@mz.co.kr>
2020-12-28 12:04:28 +0900

    Update for hiding Secret

--- src/spaceone/monitoring/conf/global_conf.py
+++ src/spaceone/monitoring/conf/global_conf.py
@@ -1,3 +1,18 @@
 CONNECTORS = {
     'GoogleCloudConnector': {}
 }
+
+LOG = {
+    'filters': {
+        'masking': {
+            'rules': {
+                'Metric.list': [
+                    'secret_data'
+                ],
+                'Metric.get_data': [
+                    'secret_data'
+                ]
+            }
+        }
+    }
+}
