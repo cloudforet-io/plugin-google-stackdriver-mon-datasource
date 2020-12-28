376eafa8fd5d2fab5ef6da16c25eead4f7c0746f
D.Kang <dkang@mz.co.kr>
2020-12-28 12:09:58 +0900

    Update for hiding Secret

--- src/spaceone/monitoring/conf/global_conf.py
+++ src/spaceone/monitoring/conf/global_conf.py
@@ -6,12 +6,18 @@ LOG = {
     'filters': {
         'masking': {
             'rules': {
+                'DataSource.verify': [
+                    'secret_data'
+                ],
                 'Metric.list': [
                     'secret_data'
                 ],
                 'Metric.get_data': [
                     'secret_data'
-                ]
+                ],
+                'Log.list': [
+                    'secret_data'
+                ],
             }
         }
     }
